71214a9456474739047c1ad977e07189a3d176d4
fher03 <feransic@gmail.com>
2025-11-12 16:20:57 -0600

    feat: Se implementan las fuentes en el archivo nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
+  app: {
+    head: {
+      link: [
+        {
+          href: "https://fonts.googleapis.com",
+          rel: "preconnect",
+        },
+        {
+          href: "https://fonts.gstatic.com",
+          rel: "preconnect",
+          crossorigin: "",
+        },
+        {
+          href: "https://fonts.googleapis.com/css2?family=Cascadia+Code:ital,wght@0,200..700;1,200..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
+          rel: "stylesheet",
+        },
+      ],
+    },
+  },
 
   modules: [
     "@nuxt/eslint",
