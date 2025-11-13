7987a7dc7a8c65f0fddac5f2f9a53ea664f13417
fher03 <feransic@gmail.com>
2025-11-12 18:35:55 -0600

    feat: Se implementa BaseLayout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
           crossorigin: "",
         },
         {
-          href: "https://fonts.googleapis.com/css2?family=Cascadia+Code:ital,wght@0,200..700;1,200..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
+          href: "https://fonts.googleapis.com/css2?family=Cascadia+Code:ital,wght@0,200..700;1,200..700&family=Inconsolata:wght@200..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
           rel: "stylesheet",
         },
       ],
