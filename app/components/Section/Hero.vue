5672a7ab33aa36eb13ab9888c5721901c0fbbbfb
fher03 <feransic@gmail.com>
2025-11-13 21:47:38 -0600

    feat: Se implementa animacion typewriter a la seccion hero y keyframe para simular cursor

--- app/components/Section/Hero.vue
+++ app/components/Section/Hero.vue
@@ -1,12 +1,32 @@
+<script setup lang="ts">
+import { useTypewriter } from "~/composable/useTypewriter";
+const roles = ['"Full-Stack Developer";', '"Product Manager";', '"Scrum Master";', '"VueJS Developer";'];
+
+const { displayedText, isPaused, startTyping } = useTypewriter(roles);
+
+onMounted(() => {
+  startTyping();
+});
+</script>
 <template>
   <section
     class="flex flex-col h-[700px] gap-10 xl:gap-7 text-center items-center justify-center md:h-[500px] xl:h-screen w-full"
   >
     <h1 class="text-6xl xl:text-8xl text-white font-bold">
-      <span class="text-red-500 mr-2 xl:mr-16">></span>Fernando Rodríguez<span class="text-red-500">_</span>
+      <span class="text-red-500 mr-2 xl:mr-16">></span>Fernando Rodríguez<span class="text-red-500 animate-blink"
+        >_</span
+      >
     </h1>
-    <h2 class="text-white text-2xl xl:text-4xl">
-      <span class="text-red-500">const</span> <span class="text-gray-400">role</span> = "Full-Stack Software Developer";
+    <h2 class="text-white text-2xl xl:text-4xl font-mono">
+      <span class="text-red-500">const</span>
+      <span class="text-gray-400"> role</span>
+      <span class="text-white"> = </span>
+      <span class="text-white">{{ displayedText }}</span>
+      <span
+        class="text-red-500 ml-1 inline-block"
+        :class="isPaused ? 'animate-blink' : 'animate-pulse'"
+        >|</span
+      >
     </h2>
     <div class="text-gray-400 text-base xl:text-2xl font-medium my-5">
       <p>// Transformando ideas en código limpio y escalable.</p>
