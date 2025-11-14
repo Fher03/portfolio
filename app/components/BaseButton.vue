0e52b9845b14a7092e77708cf1f8ebd24996f86d
fher03 <feransic@gmail.com>
2025-11-13 19:37:24 -0600

    feat: Se implementa componente BaseButton

---
+++ app/components/BaseButton.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+type ButtonColor = "primary" | "secondary" | "anchor";
+
+const props = defineProps<{
+  type?: "anchor" | "button" | "submit";
+  color?: ButtonColor;
+}>();
+
+const colorStyles: Record<ButtonColor, string> = {
+  primary: "border-red-500 text-red-500",
+  secondary: "border-gray-500 text-gray-500",
+  anchor: "border-none",
+};
+
+const hoverStyles: Record<ButtonColor, string> = {
+  primary: "bg-red-500",
+  secondary: "bg-gray-500",
+  anchor: "border-none",
+};
+</script>
+
+<template>
+  <button
+    v-if="props.type !== 'anchor'"
+    :type="props.type || 'button'"
+    class="bg-transparent border-2 px-6 py-3 text-xl group relative overflow-hidden transition-colors duration-300"
+    :class="[colorStyles[props.color || 'primary']]"
+  >
+    <span
+      class="absolute inset-0 w-0 transition-all duration-500 ease-out group-hover:w-full"
+      :class="[hoverStyles[props.color || 'primary']]"
+    ></span>
+    <span class="relative z-10 group-hover:text-white transition-colors duration-500">[<slot />]</span>
+  </button>
+  <a
+    v-else
+    href="#"
+    >[<slot />]</a
+  >
+</template>
