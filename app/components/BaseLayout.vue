84eb9fd54b78a3423d521c6f356201bd77f5e471
fher03 <feransic@gmail.com>
2025-11-13 18:18:18 -0600

    feat: Se implementa diseño responsivo para navbar

--- app/components/BaseLayout.vue
+++ app/components/BaseLayout.vue
@@ -5,6 +5,7 @@ type Links = {
   isSelected?: boolean;
 };
 
+const isSidebarOpen = ref(false);
 const links = reactive<Links[]>([
   {
     name: "Inicio",
@@ -37,12 +38,34 @@ function changeSelectedLink(index: number) {
     links[index].isSelected = true;
   }
 }
+
+function showSidebar() {
+  console.log("Se presiona");
+  isSidebarOpen.value = !isSidebarOpen.value;
+}
 </script>
 
 <template>
-  <nav class="sticky top-0 text-white flex justify-between px-40 items-center bg-[#212121] h-20 w-full">
-    <h2 class="text-2xl"><span class="text-red-500">></span> Fher03<span class="text-red-500">_dev</span></h2>
-    <ul class="flex gap-4">
+  <nav class="fixed top-0 text-white flex gap-10 px-10 md:px-20 items-center bg-[#212121] h-20 w-full">
+    <div
+      class="md:hidden"
+      @click="showSidebar"
+    >
+      <Icon
+        v-if="isSidebarOpen"
+        name="charm:cross"
+        class="text-4xl"
+      />
+      <Icon
+        v-else
+        name="charm:menu-hamburger"
+        class="text-4xl"
+      />
+    </div>
+    <h2 class="flex gap-3 text-center md:text-left w-full text-2xl">
+      <span class="text-red-500">></span> Fher03<span class="text-red-500">_dev</span>
+    </h2>
+    <ul class="hidden lg:flex items-center justify-end gap-4 w-full">
       <li
         v-for="(link, index) in links"
         :key="index"
@@ -56,4 +79,23 @@ function changeSelectedLink(index: number) {
       </li>
     </ul>
   </nav>
+  <div
+    v-if="isSidebarOpen"
+    class="fixed lg:hidden top-20 bg-[#212121] h-full w-full"
+  >
+    <ul class="flex flex-col  items-center justify-center gap-10 w-full h-full text-white">
+      <li
+        v-for="(link, index) in links"
+        :key="index"
+        class="text-2xl"
+        @click="changeSelectedLink(index)"
+      >
+        <a
+          :href="link.url"
+          :class="{ 'text-red-500': link.isSelected }"
+          >[{{ link.name }}]</a
+        >
+      </li>
+    </ul>
+  </div>
 </template>
