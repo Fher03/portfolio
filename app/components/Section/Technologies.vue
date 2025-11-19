739142dfe01bf380b85cd3503d55eb41d4df7cad
fher03 <feransic@gmail.com>
2025-11-18 18:09:15 -0600

    feat: Se implementan tecnologias a la seccion tech

--- app/components/Section/Technologies.vue
+++ app/components/Section/Technologies.vue
@@ -1,3 +1,55 @@
+<script setup lang="ts">
+type Technology = {
+  name: string;
+  icon: string;
+};
+
+const technologies: Record<"frontend" | "backend" | "devOps", Technology[]> = {
+  frontend: [
+    {
+      name: "VueJS",
+      icon: "fontisto:vuejs",
+    },
+    {
+      name: "TailwindCSS",
+      icon: "mdi:tailwind",
+    },
+    {
+      name: "HTML",
+      icon: "fontisto:html5",
+    },
+  ],
+  backend: [
+    {
+      name: "NodeJS",
+      icon: "fontisto:nodejs",
+    },
+    {
+      name: "PostgreSQl",
+      icon: "akar-icons:postgresql-fill",
+    },
+    {
+      name: "redis",
+      icon: "fontisto:redis",
+    },
+  ],
+  devOps: [
+    {
+      name: "VS Code",
+      icon: "fontisto:visual-studio",
+    },
+    {
+      name: "Docker",
+      icon: "fontisto:docker",
+    },
+    {
+      name: "Terminal",
+      icon: "si:terminal-fill",
+    },
+  ],
+};
+</script>
+
 <template>
   <section class="flex flex-col gap-10 h-fit py-20 w-full">
     <div class="w-full flex flex-col gap-2">
@@ -7,15 +59,47 @@
       <p class="text-gray-500 text-xl">// Herramientas y tecnologias que domino.</p>
     </div>
     <div class="flex flex-col gap-15">
-      <div class="flex gap-10 flex-col">
-        <h3 class="text-3xl text-white font-bold"><span class="text-red-500 font-semibold">//</span> DevOps & Tools</h3>
-        <div class="flex flex-col items-center justify-center gap-5 p-10 border-2 border-gray-500/40 w-full md:w-xs">
+      <h3 class="text-3xl text-white font-bold"><span class="text-red-500 font-semibold">//</span> FrontEnd</h3>
+      <div class="flex flex-col md:flex-row justify-center gap-10">
+        <div
+          v-for="(tech, index) in technologies.frontend"
+          :key="index"
+          class="flex flex-col items-center justify-center gap-5 p-10 border-2 border-gray-500/40 w-full md:w-xs"
+        >
           <Icon
-            name="fontisto:vuejs"
+            :name="tech.icon"
             class="text-white text-8xl"
           />
-          <p class="text-gray-500 text-xl">VueJS</p>
+          <p class="text-red-500 text-xl">{{ tech.name }}</p>
         </div>
+      </div>
+    </div>
+    <h3 class="text-3xl text-white font-bold"><span class="text-red-500 font-semibold">//</span> BackEnd</h3>
+    <div class="flex flex-col md:flex-row justify-center gap-10">
+      <div
+        v-for="(tech, index) in technologies.backend"
+        :key="index"
+        class="flex flex-col items-center justify-center gap-5 p-10 border-2 border-gray-500/40 w-full md:w-xs"
+      >
+        <Icon
+          :name="tech.icon"
+          class="text-white text-8xl"
+        />
+        <p class="text-red-500 text-xl">{{ tech.name }}</p>
+      </div>
+    </div>
+    <h3 class="text-3xl text-white font-bold"><span class="text-red-500 font-semibold">//</span> DevOps & Tools</h3>
+    <div class="flex flex-col md:flex-row justify-center gap-10">
+      <div
+        v-for="(tech, index) in technologies.devOps"
+        :key="index"
+        class="flex flex-col items-center justify-center gap-5 p-10 border-2 border-gray-500/40 w-full md:w-xs"
+      >
+        <Icon
+          :name="tech.icon"
+          class="text-white text-8xl"
+        />
+        <p class="text-red-500 text-xl">{{ tech.name }}</p>
       </div>
     </div>
   </section>
