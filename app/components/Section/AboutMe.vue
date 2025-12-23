932a63c1d4eaa9560052113a9f2f1896ff4bd8bd
Fher03 <feransic@gmail.com>
2025-12-23 10:09:08 -0600

    feat: Se implementa seccion About me

---
+++ app/components/Section/AboutMe.vue
@@ -0,0 +1,18 @@
+<template>
+  <section id="aboutme" class="flex flex-col gap-10 h-fit py-20 w-full">
+    <div class="w-full flex flex-col gap-2">
+      <h2 class="text-4xl text-white">
+        <span class="text-red-500 mr-2">></span>Sobre mí<span class="text-red-500 animate-blink">_</span>
+      </h2>
+      <p class="text-gray-500 text-xl">// Mi trayectoria profesional.</p>
+    </div>
+    <div class="flex">
+
+        <div>
+            
+        </div>
+        <p class="text-xl  text-white">Como desarrollador Full Stack en la recta final de mi carrera universitaria, poseo una visión global del ciclo de vida de un proyecto de software. Disfruto el reto de trabajar en ambos lados de la moneda: desde la lógica compleja y el manejo de datos en el servidor, hasta la creación de interfaces visuales intuitivas y dinámicas que cautivan al usuario final.
+            Mi filosofía de trabajo se basa en la mejora constante. No solo busco escribir código que funcione, sino código que sea elegante y fácil de mantener. Tengo un interés genuino en la automatización de procesos y en encontrar soluciones innovadoras a problemas cotidianos. Estoy siempre en busca de nuevas herramientas y metodologías que me permitan seguir desarrollando aplicaciones web de alto impacto y continuar creciendo profesionalmente en esta industria en constante cambio</p>
+    </div>
+  </section>
+</template>
